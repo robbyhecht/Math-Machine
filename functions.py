4619aa3bd814e53c65d8788f68ff65f9d3e69e9d
robbyhecht <robbyhecht@gmail.com>
2019-07-11 16:20:01 -0500

    entry must be > 0. Currently can only be an integer value

--- functions.py
+++ functions.py
@@ -22,41 +22,40 @@ def triangle():
             sideType = input('Please enter "s" for side or "h" for hypotenuse: ')
         if sideType == 'hypotenuse' or sideType == 'h':
             a = input('enter side 1: ')
-            while a.isdigit() == False:
+            while a.isdigit() == False or int(a) == 0:
                 a = input('Please enter a positive integer value for side 1: ')
             b = input('enter side 2: ')
-            while b.isdigit() == False:
+            while b.isdigit() == False or int(b) == 0:
                 b = input('Please enter a positive integer value for side 2: ')
-            a = int(a)
-            b = int(b)
+            a = float(a)
+            b = float(b)
             print('The hypotenuse is ' + str(math.sqrt(a*a + b*b)) + '!')
         if sideType == 'side' or sideType == 's':
             c = (input('Enter the hypotenuse: '))
-            while c.isdigit() == False:
+            while c.isdigit() == False or int(c) == 0:
                   c = (input('Please enter a positive integer value for the hypotenuse: '))
             a = input('Enter the side: ')
-            while a.isdigit() == False:
+            while a.isdigit() == False or int(a) == 0:
                 a = input('Please enter a positive integer value for the side: ')
-            c = int(c)
-            a = int(a)
+            c = float(c)
+            a = float(a)
             print('The other side is ' + str(math.sqrt(c*c - a*a)) + '!')
-
     if calc == 'angle' or calc == 'a':
         a = input('Enter angle 1: ')
-        while a.isnumeric() == False or float(a)<=0:
+        while a.isdigit() == False or int(a) == 0:
             a = input('Please enter a positive integer value for angle 1: ')
         b = input('Enter angle 2: ')
-        while b.isnumeric() == False or float(b)<=0:
+        while b.isdigit() == False or int(b) == 0:
             b = input('Please enter a positive integer value for angle 2: ')
         a = float(a)
         b = float(b)
         while a + b >= 180:
             print('angle 1 + angle 2 must be less than 180!')
             a = input('Enter angle 1: ')
-            while a.isnumeric() == False or float(a)<=0:
+            while a.isdigit() == False or int(a) == 0:
                 a = input('Please enter a positive integer value for angle 1: ')
             b = input('Enter angle 2: ')
-            while b.isnumeric() == False or float(b)<=0:
+            while b.isdigit() == False or int(b) == 0:
                 b = input('Please enter a positive integer value for angle 2: ')
             a = float(a)
             b = float(b)
@@ -68,33 +67,33 @@ def rectangle():
         calc = input('Please enter "a" for area, "p" for perimeter or "d" for diagonal: ')
     if calc == 'area' or calc == 'a':
         l = input('Enter the length: ')
-        while l.isdigit() == False:
+        while l.isdigit() == False or int(l) == 0:
             l = input('Please enter a positive integer value for length: ')
         w = input('Enter the width: ')
-        while w.isdigit() == False:
+        while w.isdigit() == False or int(w) == 0:
             w = input('Please enter a positive integer value for width: ')
-        l = int(l)
-        w = int(w)
+        l = float(l)
+        w = float(w)
         print('The area is ' + str(l*w) + '!')
     if calc == 'perimeter' or calc == 'p':
         l = input('Enter the length: ')
-        while l.isdigit() == False:
+        while l.isdigit() == False or int(l) == 0:
             l = input('Please enter a positive integer value for length: ')
         w = input('Enter the width: ')
-        while w.isdigit() == False:
+        while w.isdigit() == False or int(w) == 0:
             w = input('Please enter a positive integer value for width: ')
-        l = int(l)
-        w = int(w)
+        l = float(l)
+        w = float(w)
         print('The perimeter is ' + str(2*l + 2*w) + '!')
     if calc == 'diagonal' or calc == 'd':
         l = input('Enter the length: ')
-        while l.isdigit() == False:
+        while l.isdigit() == False or int(l) == 0:
             l = input('Please enter a positive integer value for length: ')
         w = input('Enter the width: ')
-        while w.isdigit() == False:
+        while w.isdigit() == False or int(w) == 0:
             w = input('Please enter a positive integer value for width: ')
-        l = int(l)
-        w = int(w)
+        l = float(l)
+        w = float(w)
         print('The diagonal is ' + str(math.sqrt(l*l + w*w)) + '!')
 
 def circle():
@@ -103,15 +102,15 @@ def circle():
         calc = input('Please enter "c" for circumference, "a" for area, or "d" for diameter: ')
     if calc == 'circumference' or calc == 'c':
         r = input('Enter the radius: ')
-        while r.isdigit() == False:
+        while r.isdigit() == False or int(r) == 0:
             r = input('Please enter a positive integer value for radius: ')
-        r = int(r)
+        r = float(r)
         print('The circumference is ' + str(math.pi * (2*r)) + '!')
     if calc == 'area' or calc == 'a':
         r = input('Enter the radius: ')
-        while r.isdigit() == False:
+        while r.isdigit() == False or int(r) == 0:
             r = input('Please enter a positive integer value for radius: ')
-        r = int(r)
+        r = float(r)
         print('The area is ' + str(math.pi * (r*r)) + '!')
     if calc == 'diameter' or calc == 'd':
         asset = input('Do you know the area or circumference? Please enter "a" or "c": ')
@@ -119,15 +118,15 @@ def circle():
             asset = input('Please enter "a" for area or "c" for circumference: ')
         if asset == 'area' or asset == 'a':
             a = input('Please enter the area: ')
-            while a.isdigit() == False:
+            while a.isdigit() == False or int(a) == 0:
                 a = input('Please enter a positive integer value for area: ')
-            a = int(a)
+            a = float(a)
             print('The diameter is ' + str(math.sqrt((a / math.pi)) * 2) + '!')
         if asset == 'circumference' or asset == 'c':
             c = input('Please enter the circumference: ')
-            while c.isdigit() == False:
+            while c.isdigit() == False or int(c) == 0:
                 c = input('Please enter a positive integer value for circumference: ')
-            c = int(c)
+            c = float(c)
             print('The diameter is ' + str(c / math.pi) + '!')
 
 def sphere():
@@ -136,15 +135,15 @@ def sphere():
         calc = input('Please enter "s" for surface area, "v" for volume or "d" for diameter: ')
     if calc == 'surface area' or calc == 'area' or calc == 's':
         d = input('Enter the diameter: ')
-        while d.isdigit() == False:
+        while d.isdigit() == False or int(d) == 0:
             d = input('Please enter a positive integer value for diameter: ')        
-        d = int(d)
+        d = float(d)
         print('The surface area is ' + str(4 * math.pi * (d/2)**2) + '!')
     if calc == 'volume' or calc == 'v':
         d = input('Enter the diameter: ')
-        while d.isdigit() == False:
+        while d.isdigit() == False or int(d) == 0:
             d = input('Please enter a positive integer value for diameter: ') 
-        d = int(d)
+        d = float(d)
         print('The volume is ' + str((4/3) * math.pi * ((d/2)**3)) + '!')
     if calc == 'diameter' or calc == 'd':
         asset = input('Do you know the surface area or volume? Please enter "s" or "v": ')
@@ -152,16 +151,16 @@ def sphere():
             asset = input('Please enter "s" for surface area or "v" for volume ')
         if asset == 'surface area' or asset == 'area' or asset == 's':
             a = input('Enter the surface area: ')
-            while a.isdigit() == False:
+            while a.isdigit() == False or int(a) == 0:
                 a = input('Please enter a positive integer value for volume: ') 
-            a = int(a)
+            a = float(a)
             print('The diameter is ' + str(math.sqrt(a / math.pi)) + '!')
 
         if asset == 'volume' or asset == 'v':
             v = input('Enter the volume: ')
-            while v.isdigit() == False:
+            while v.isdigit() == False or int(v) == 0:
                 v = input('Please enter a positive integer value for volume: ') 
-            v = int(v)
+            v = float(v)
             print('The diameter is ' + str((6*(v / math.pi))**(1/3)) + '!')
 
 def box():
@@ -170,76 +169,43 @@ def box():
         calc = input('Please enter "s" for surface area, "v" for volume or "d" for diagonal: ')
     if calc == 'surface area' or calc == 'area' or calc == 's':
         l = input('Enter the length: ')
-        while l.isdigit() == False:
+        while l.isdigit() == False or int(l) == 0:
             l = input('Please enter a positive integer value for length: ') 
         w = input('Enter the width: ')
-        while w.isdigit() == False:
+        while w.isdigit() == False or int(w) == 0:
             w = input('Please enter a positive integer value for width: ')
         h = input('Enter the height: ')
-        while h.isdigit() == False:
+        while h.isdigit() == False or int(h) == 0:
             h = input('Please enter a positive integer value for height: ')
-        l = int(l)
-        w = int(w)
-        h = int(h)
+        l = float(l)
+        w = float(w)
+        h = float(h)
         print('The surface area is ' + str(2 * (h*w) + 2 * (h*l) + 2 * (w*l)) + '!')
     if calc == 'volume' or calc == 'v':
         l = input('Enter the length: ')
-        while l.isdigit() == False:
+        while l.isdigit() == False or int(l) == 0:
             l = input('Please enter a positive integer value for length: ') 
         w = input('Enter the width: ')
-        while w.isdigit() == False:
+        while w.isdigit() == False or int(w) == 0:
             w = input('Please enter a positive integer value for width: ')
         h = input('Enter the height: ')
-        while h.isdigit() == False:
+        while h.isdigit() == False or int(h) == 0:
             h = input('Please enter a positive integer value for height: ')
-        l = int(l)
-        w = int(w)
-        h = int(h)
+        l = float(l)
+        w = float(w)
+        h = float(h)
         print('The volume is ' + str(l * w * h) + '!')
     if calc == 'diagonal' or calc == 'd':
         l = input('Enter the length: ')
-        while l.isdigit() == False:
+        while l.isdigit() == False or int(l) == 0:
             l = input('Please enter a positive integer value for length: ') 
         w = input('Enter the width: ')
-        while w.isdigit() == False:
+        while w.isdigit() == False or int(w) == 0:
             w = input('Please enter a positive integer value for width: ')
         h = input('Enter the height: ')
-        while h.isdigit() == False:
+        while h.isdigit() == False or int(h) == 0:
             h = input('Please enter a positive integer value for height: ')
-        l = int(l)
-        w = int(w)
-        h = int(h)
+        l = float(l)
+        w = float(w)
+        h = float(h)
         print('The diagonal is ' + str(math.sqrt((math.sqrt(l**2 + w**2))**2 + h**2)) + '!')
-
-# def box():
-#     calc = input('Do you want me to find the surface area or volume? Please enter "s" or "v"')
-#     while calc != 'surface area' and calc != 'area' and calc != 's' and calc != 'volume' and calc != 'v':
-#         calc = input('Please enter "s" for surface area or "v" for volume: ')
-#     if calc == 'surface area' or calc == 'area' or calc == 's':
-#         l = input('Enter the length: ')
-#         while l.isdigit() == False:
-#             l = input('Please enter a positive integer value for length: ') 
-#         w = input('Enter the width: ')
-#         while w.isdigit() == False:
-#             w = input('Please enter a positive integer value for width: ')
-#         h = input('Enter the height: ')
-#         while h.isdigit() == False:
-#             h = input('Please enter a positive integer value for height: ')
-#         l = int(l)
-#         w = int(w)
-#         h = int(h)
-#         print('The surface area is ' + str(2 * (h*w) + 2 * (h*l) + 2 * (w*l)) + '!')
-#     if calc == 'volume' or calc == 'v':
-#         l = input('Enter the length: ')
-#         while l.isdigit() == False:
-#             l = input('Please enter a positive integer value for length: ') 
-#         w = input('Enter the width: ')
-#         while w.isdigit() == False:
-#             w = input('Please enter a positive integer value for width: ')
-#         h = input('Enter the height: ')
-#         while h.isdigit() == False:
-#             h = input('Please enter a positive integer value for height: ')
-#         l = int(l)
-#         w = int(w)
-#         h = int(h)
-#         print('The volume is ' + str(l * w * h) + '!')
